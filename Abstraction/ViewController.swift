da29331ebd24c15859a9c308db0d0e7bd0329e28
Popcornblaster <lucinablaster@gmail.com>
2018-12-04 12:15:12 -0700

    Initial Commit

---
+++ Abstraction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Abstraction
+//
+//  Created by Baugh, Tyler on 12/4/18.
+//  Copyright © 2018 Baugh, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
