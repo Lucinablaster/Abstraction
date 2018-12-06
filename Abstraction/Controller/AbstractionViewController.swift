3c6d84e3eb3a5c2520d4bef2bf587472eb195cc1
Popcornblaster <lucinablaster@gmail.com>
2018-12-06 12:31:46 -0700

    Organized, added view controllers, added labels, images, added unfinished code to the view controller.

---
+++ Abstraction/Controller/AbstractionViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AbstractionViewController.swift
+//  Abstraction
+//
+//  Created by Baugh, Tyler on 12/6/18.
+//  Copyright © 2018 Baugh, Tyler. All rights reserved.
+//
+
+import UIKit
+
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
+    {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    //MARK: - Data Members
+    private lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Swift"),
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "LogicalGate")
+        ]
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
